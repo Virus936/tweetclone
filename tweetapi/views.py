952a63c17e6997a061844c2db92a9ee615afd3e6
Virus936 <lok.vira@gmail.com>
2021-04-08 15:19:37 +0200

    view modifié afin d'accepter les posts

--- tweetapi/views.py
+++ tweetapi/views.py
@@ -6,3 +6,6 @@ from tweetapi.serializer import TweetSerializer
 class TweetList(generics.ListCreateAPIView):
     queryset = Tweet.objects.all()
     serializer_class = TweetSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
