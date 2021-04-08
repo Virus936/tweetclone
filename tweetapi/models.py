4257fad3456fbcda696bb64b94679b0215e8e01a
Virus936 <lok.vira@gmail.com>
2021-04-08 12:14:24 +0200

    migration, test, et admin

--- tweetapi/models.py
+++ tweetapi/models.py
@@ -5,13 +5,23 @@ User = get_user_model()
 
 
 class Tweet(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author")
     content = models.TextField("auteur du tweet", blank=True, null=True)
     picture = models.ImageField()
     date_created = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(User, through="Like")
 
     def __repr__(self):
-        return f"{self.author} : {self.content[30]}..."
+        return f"{self.author} : {self.content}..."
 
     def __str__(self):
-        return f"{self.author} : {self.content[30]}..."
+        return f"{self.author} : {self.content}..."
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="liker")
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE, related_name="liked")
+    timestamp = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ["user", "tweet"]
--- tweetapi/tests.py
+++ tweetapi/tests.py
@@ -1,2 +1,30 @@
 from django.test import TestCase
-from .models import Tweet
+from .models import Tweet, Like
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class LikeTest(TestCase):
+    def test_if_a_like_work(self):
+        user1 = User.objects.create(username="vira")
+        tweet1 = Tweet.objects.create(author=user1, content="here is my first tweet")
+        Like.objects.create(user=user1, tweet=tweet1)
+        self.assertEqual(user1, tweet1.likes.first())
+
+    def test_if_two_likes_work(self):
+        user1 = User.objects.create(username="vira")
+        user2 = User.objects.create(username="jule")
+        tweet1 = Tweet.objects.create(author=user1, content="here is my first tweet")
+        Like.objects.create(user=user1, tweet=tweet1)
+        Like.objects.create(user=user2, tweet=tweet1)
+        self.assertEqual(len(tweet1.likes.all()), 2)
+
+    def test_the_list_of_users_likes(self):
+        user1 = User.objects.create(username="vira")
+        print(user1)
+        user2 = User.objects.create(username="Jule")
+        tweet1 = Tweet.objects.create(author=user1, content="here is my first tweet")
+        print(tweet1)
+        tweet2 = Tweet.objects.create(author=user1, content="here is my second tweet")
+        self.assertEqual(len(user1.tweet_set.all()), 2)
