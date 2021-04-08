958da2e23a0c4213eb0532da3b349f93e4c489f1
Virus936 <lok.vira@gmail.com>
2021-04-08 10:00:12 +0200

    creation du model Tweet

--- tweetapi/models.py
+++ tweetapi/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Tweet(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField("auteur du tweet", blank=True, null=True)
+    picture = models.ImageField()
+    date_created = models.DateTimeField(auto_now=True)
+
+    def __repr__(self):
+        return f"{self.author} : {self.content[30]}..."
+
+    def __str__(self):
+        return f"{self.author} : {self.content[30]}..."
