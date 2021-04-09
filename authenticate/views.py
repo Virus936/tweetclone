dd85f7dcf82a7f331c5dd4e514cacb4dd2ffdc06
Virus936 <lok.vira@gmail.com>
2021-04-09 09:14:13 +0200

    register fonctionality

--- authenticate/views.py
+++ authenticate/views.py
@@ -6,3 +6,15 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 class MyObtainTokenPairView(TokenObtainPairView):
     permission_classes = (AllowAny,)
     serializer_class = MyTokenObtainPairSerializer
+
+
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+from rest_framework import generics
+
+
+class RegisterView(generics.CreateAPIView):
+
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
