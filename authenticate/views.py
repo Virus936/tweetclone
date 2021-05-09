e2319b7a3093f4240c3dbf81d07514a954271bf3
Virus936 <lok.vira@gmail.com>
2021-05-09 15:52:50 +0200

    refactoring

--- authenticate/views.py
+++ authenticate/views.py
@@ -1,16 +1,14 @@
 from .serializers import MyTokenObtainPairSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+from rest_framework import generics
 
 
 class MyObtainTokenPairView(TokenObtainPairView):
     permission_classes = (AllowAny,)
     serializer_class = MyTokenObtainPairSerializer
-
-
-from django.contrib.auth.models import User
-from .serializers import RegisterSerializer
-from rest_framework import generics
 
 
 class RegisterView(generics.CreateAPIView):
