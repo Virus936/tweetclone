c907b1584dba7612c4dec958b232fb8c84eb0583
Virus936 <lok.vira@gmail.com>
2021-05-26 23:22:10 +0200

    like or not functionality

--- tweetapi/serializer.py
+++ tweetapi/serializer.py
@@ -5,10 +5,15 @@ from userprofile.serializers import UserSerializer
 
 class TweetSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
+    likeornot = serializers.SerializerMethodField()
+
+    def get_likeornot(self, obj):
+        current_user = self.context["request"].user
+        return current_user in obj.likes.all()
 
     class Meta:
         model = Tweet
-        fields = ["id", "author", "picture", "content", "numlike"]
+        fields = ["id", "author", "picture", "content", "numlike", "likeornot"]
 
 
 # Look a this link  to improve the author display
